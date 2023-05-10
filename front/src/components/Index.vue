a769328fc02425eeaeb26e828c2af9185c744968
Euphoria-zy <2978766226@qq.com>
2023-05-10 15:54:07 +0800

    2023/05/10

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -2,37 +2,59 @@
   <div style="height: 100%; width: 100%;">
     <el-container style="width: 100%;height: 100%;align-content: center;">
       <el-main style="width: 60%;height: 100%;">
-      <el-row class="first-row">
+        <el-row class="first-row">
           <el-col :span="10">
-          <el-input v-model="param.num" placeholder="请输入结点个数："></el-input>
-        </el-col>
+            <el-input v-model="param.num" placeholder="请输入结点个数："></el-input>
+          </el-col>
           <el-col :span="14">
-          <el-button @click="getImage" type="primary">计算</el-button>
-        </el-col>
-      </el-row>
+            <el-button @click="getImage" type="primary">Calculate</el-button>
+          </el-col>
+        </el-row>
         <el-row :gutter="24" class="second-row" style="margin: auto;" v-loading="loading">
           <el-col :span="8" class="second-row-left" v-for="(item, index) in imageUrl">
             <el-card :body-style="{ width: '100%', height: '100%', padding: '0', margin: '0' }">
-              <el-image
-              :src="imageUrl[index]"
-              :preview-src-list="previewList"
-              :initial-index="index"
-              @click="vbs(imageUrl[index])">
+              <el-image :src="imageUrl[index]" :preview-src-list="previewList" :initial-index="index"
+                @click="vbs(imageUrl[index])">
                 <template #error>
                   <div class="image-slot">
                     <el-empty />
                   </div>
                 </template>
               </el-image>
-              
-              <div style="text-align: center;">
-                <span>Yummy hamburger</span>
+
+              <div style="text-align: center;" v-if="index != 0">
+                <span>可信信息覆盖面积：{{ areas[index] }}</span>
+              </div>
+              <div style="text-align: center;" v-else>
+                <span>初试状态</span>
               </div>
             </el-card>
           </el-col>
-      </el-row>
-    </el-main>
-  </el-container>
+        </el-row>
+      </el-main>
+    </el-container>
+    <div style="width: 150px;height: 200px;position: fixed;top: calc(10% + 20px);left: 10px;">
+      <div style="display: flex;align-items: center;margin-bottom: 20px;" v-for="(item, index) in iconConfig">
+        <div style="width: 80px;justify-content: center;display: flex;">
+          <template v-if="item.type == 1">
+            <div style="width: 15px;height: 15px; border-radius: 50%;" :style="{background: item.color}"></div>
+          </template>
+          <template v-else-if="item.type == 2">
+            <div :style="{color: item.color}"><span class="iconfont" style="font-size: 25px;">&#xe647;</span></div>
+          </template>
+          <template v-else-if="item.type == 3">
+            <div :style="{borderBottom: `3px dashed ${item.color}`, width: '30px'}"></div>
+          </template>
+          <template v-else-if="item.type == 4">
+            <div style="width: 30px;height: 30px;border: 1px dashed black;"></div>
+          </template>
+          <template v-else-if="item.type == 5">
+            <div :style="{color: item.color, fontSize: '30px'}">*</div>
+          </template>
+        </div>
+        <div :style="{color: item.color, fontSize: '12px'}" style="flex: 1;text-align: center;">{{ item.name }}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -40,9 +62,9 @@ import API from "../api/api_file"
 export default {
   created() {
     //this.getImage();
-    if(this.imageUrl.length === 0) {
-      this.loading = true;
-    }
+    // if (this.imageUrl.length === 0) {
+    //   this.loading = true;
+    // }
   },
   data() {
     return {
@@ -51,12 +73,56 @@ export default {
       },
       loading: false,
       imageUrl: [
-         //"https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+        //"https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
       ],
-      previewList:[
-      "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      areas: [0,1,2,3,4,5],
+      previewList: [
+        "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
       ],
-      showImage: false
+      showImage: false,
+      drawer: false,
+      iconConfig: [
+        {
+          type: 1,
+          color: "red",
+          name: "激活状态"
+        },
+        {
+          type: 1,
+          color: "blue",
+          name: "中继状态"
+        },
+        {
+          type: 1,
+          color: "yellow",
+          name: "睡眠状态"
+        },
+        {
+          type: 1,
+          color: "black",
+          name: "失效状态"
+        },
+        {
+          type: 2,
+          color: "green",
+          name: "汇聚结点"
+        },
+        {
+          type: 3,
+          color: "purple",
+          name: "可通信链路"
+        },
+        {
+          type: 4,
+          color: "",
+          name: "重建区域"
+        },
+        {
+          type: 5,
+          color: "green",
+          name: "重建点"
+        }
+      ]
     }
   },
   methods: {
@@ -65,12 +131,14 @@ export default {
       this.loading = true;
       API.getImage(this.param).then(
         function (res) {
-          var base64 = res.data;
+          var base64 = res.data.images;
+          var areas = res.data.areas;
           for (let index = 0; index < base64.length; index++) {
             const element = "data:image/png;base64," + base64[index];
             that.imageUrl.push(element);
             that.loading = false;
           }
+          areas.unshift(0);
         }
       )
     },
@@ -144,8 +212,8 @@ export default {
   justify-content: center;
   padding: 0;
 }
+
 .el-image img {
   margin: 0 auto;
 }
-
 </style>
